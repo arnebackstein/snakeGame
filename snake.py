62bdd5f5aadb9a5c6e0feaa94e7ff7a4b496fa2a
luisroder <luisroder@hotmail.de>
2021-09-23 19:58:46 +0200

    Added direction to snake

--- snake.py
+++ snake.py
@@ -1,10 +1,14 @@
+from Directions import Direction
+
+
 class Snake:
-    def __init__(self, body: list(tuple), direction):
+    def __init__(self, body: list[tuple], direction: Direction):
+        # Body format = [(pos_x, pos_y)]
         self.body = body
         self.direction = direction
 
-    def getSnake(self):
+    def get_snake(self):
         return self.body
 
-    def setDirection(self, direction):
+    def set_direction(self, direction):
         self.direction = direction
