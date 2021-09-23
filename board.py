bc7f4ca677c4db713eaf65ed665c3a6d67149be3
Arne Backstein <arne.backstein@gmail.com>
2021-09-23 21:16:32 +0200

    ~ Board

--- board.py
+++ board.py
@@ -12,12 +12,13 @@ class Board(object):
     def __init__(self):
         pygame.init()
 
-        self.display = pygame.display.set_mode((config.board['width'], config.board['width']))
+        self.display = pygame.display.set_mode((config.board['width'], config.board['height']))
         pygame.display.update()
         pygame.display.set_caption('SUPER CRAZY SHIT')
 
         self.clock = pygame.time.Clock()
         self.snake = Snake([Position(300, 300)], Direction.UP)
+        self.board = [[None] * int(config.board['height'] / 10)] * int(config.board['width'] / 10)
 
     def start_game_loop(self) -> None:
         game_over = False
@@ -45,10 +46,17 @@ class Board(object):
             self.snake.move()
             self.clock.tick(10)
 
+    def add_to_board(self, position: Position) -> bool:
+        # TODO implement
+        return False;
+
+    def remove_from_board(self, position: Position) -> bool:
+        # TODO implement
+        return False;
+
     def spawn_food(self):
         x = int(random.uniform(0, config.board['width']))
         y = int(random.uniform(0, config.board['height']))
 
     def quit(self):
         pygame.quit()
-
