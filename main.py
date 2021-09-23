f2e333b2597aa8f9fb2fa5531787edf6a324e886
Arne Backstein <arne.backstein@gmail.com>
2021-09-23 19:52:43 +0200

    Direction Enum

--- main.py
+++ main.py
@@ -1,49 +1,53 @@
 import pygame
+
+from Directions import Direction
+from snake import Snake
+
 pygame.init()
-dis=pygame.display.set_mode((800,600))
+dis = pygame.display.set_mode((800, 600))
 pygame.display.update()
 pygame.display.set_caption('SUPER CRAZY SHIT')
 
-#colors
-green = (0,255,0)
-red = (255,0,0)
+# colors
+green = (0, 255, 0)
+red = (255, 0, 0)
 white = (255, 255, 255)
 black = (0, 0, 0)
 
 x1 = 300
 y1 = 300
 
-x1_change = 0       
+x1_change = 0
 y1_change = 0
 
-game_over=False
+game_over = False
 
 clock = pygame.time.Clock()
 
+snake = Snake();
+
 while not game_over:
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            game_over=True
+        if event.type == pygame.QUIT:
+            game_over = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x1_change = -10
-                y1_change = 0
+                snake.setDirection(Direction.LEFT)
             elif event.key == pygame.K_RIGHT:
-                x1_change = 10
-                y1_change = 0
+                snake.setDirection(Direction.RIGHT)
             elif event.key == pygame.K_UP:
-                y1_change = -10
-                x1_change = 0
+                snake.setDirection(Direction.UP)
             elif event.key == pygame.K_DOWN:
-                y1_change = 10
-                x1_change = 0
-    
-    x1 += x1_change
-    y1 += y1_change 
+                snake.setDirection(Direction.DOWN)
+
     dis.fill(white)
-    pygame.draw.rect(dis, black, [x1, y1, 10, 10])
+
+    for part in snake.getSnake():
+        pygame.draw.rect(dis, black, [part[0], part[1], 10, 10])
 
     pygame.display.update()
+
+    snake.move();
 
     clock.tick(30)
 # Hallo
